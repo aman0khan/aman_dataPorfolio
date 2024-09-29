a0af9e343a81100e503d5b96a2fff04958b34d85
aman khan <amankhan@amans-MacBook-Air.local>
2024-09-29 23:08:46 +0530

    Initial commit

---
+++ app/about-section/About.tsx
@@ -0,0 +1,114 @@
+import SongCarousel from "./SongCarousel";
+import "../animations/animate.css";
+import AnimatedBody from "../animations/AnimatedBody";
+import AnimatedTitle from "../animations/AnimatedTitle";
+
+const About = () => {
+  return (
+    <section
+      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56"
+      id="about"
+    >
+      <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
+        <AnimatedTitle
+          text={
+            "I MAKE DATA BEAUTIFUL AND UNDERSTANDABLE."
+          }
+          className={
+            "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
+          }
+          wordSpace={"mr-[14px]"}
+          charSpace={"mr-[0.001em]"}
+        />
+
+        <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
+          <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
+            <AnimatedBody
+              text={
+                "My passion lies in creating beautiful dashboard that solves business problems and positively impacts people’s lives."
+              }
+            />
+            <AnimatedBody
+              text={
+                "Moreover, my expertise in Power BI enables me to craft compelling visualizations that communicate complex findings with clarity and impact"
+              }
+              className={"hidden"}
+            />
+            <AnimatedBody
+              text={
+                "In addition to my technical prowess, my proficiency in SQL allows me to seamlessly access and manipulate data from diverse sources, ensuring data integrity and reliability throughout the analytical process"
+              }
+            />
+            <AnimatedBody
+              text={
+                "Furthermore, my adeptness with other essential tools such as scikit-learn, Pandas and Power BI further enhances my capabilities in tackling a wide range of analytical challenges."
+              }
+            />
+            <AnimatedBody
+              text={
+                "Driven by a passion for innovation and a commitment to driving business success through data-driven insights, I am eager to contribute my expertise to dynamic teams and projects that demand excellence in machine learning engineering and data analysis."
+              }
+            />
+          </div>
+
+          <div className="mb-24 flex w-[100%] flex-col gap-4 text-[18px] font-normal leading-relaxed tracking-wide text-[#e4ded7]/80 sm:mb-32 md:mb-40 md:gap-6 md:text-[16px] md:leading-normal lg:mt-0 lg:mb-16 lg:max-w-[30%] lg:text-[18px]">
+            <div className="flex flex-col gap-4 md:gap-3">
+              <AnimatedTitle
+                text={"SQL, Python"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "Subquery, CTE, Window function, Stored Procedure, NumPy, Pandas"
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <AnimatedTitle
+                text={"Power BI"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "Data Modeling, Dax, Power BI Service, Analytics & Insights, Dashboard"
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <AnimatedTitle
+                text={"MS Excel"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "Advance Formulae, Pivot Table, Pivot Chart, Power Pivot and Dashboard"
+                }
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28">
+          <SongCarousel />
+          <AnimatedBody
+            text="A few songs I can recommend if you're looking for some fresh tunes :)"
+            className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px] "
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
