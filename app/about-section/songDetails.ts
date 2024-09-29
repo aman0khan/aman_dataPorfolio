d847c39d84ed6e183fe8049182f40286c95aba0a
aman khan <amankhan@amans-MacBook-Air.local>
2024-09-29 22:57:54 +0530

    Initial commit

---
+++ app/about-section/songDetails.ts
@@ -0,0 +1,69 @@
+export type SongProps = {
+  title: string;
+  artist: string;
+  image: string;
+  link: string;
+};
+
+export const songs = [
+  {
+    title: "Lie Again",
+    artist: "Giveon",
+    image: require(".//../../public/songs/lie-again.jpeg"),
+    link: "https://open.spotify.com/album/11q4Tt1RzwrFzF2Vddc2yO",
+  },
+  {
+    title: "Slim shaddy",
+    artist: "Eminem",
+    image: require(".//../../public/songs/martins-sofa.jpg"),
+    link: "https://open.spotify.com/track/3yfqSUWxFvZELEM4PmlwIR",
+  },
+  {
+    title: "Aam jahe munde",
+    artist: "Parmish Verma",
+    image: require(".//../../public/songs/let-me-go.jpg"),
+    link: "https://open.spotify.com/track/32VAw8DL9ixX5XSW2VLcZ4",
+  },
+  {
+    title: "One Up",
+    artist: "Central Cee",
+    image: require(".//../../public/songs/one-up.png"),
+    link: "https://open.spotify.com/album/51A9bnCs9oq6vjFZIDza97",
+  },
+  {
+    title: "Sunflower",
+    artist: "Post Malone",
+    image: require(".//../../public/songs/glitter.jpg"),
+    link: "https://open.spotify.com/track/3KkXRkHbMCARz0aVfEt68P",
+  },
+  {
+    title: "prasthanam",
+    artist: "sanjay Dutt",
+    image: require(".//../../public/songs/lemme-land.jpg"),
+    link: "https://open.spotify.com/album/46GHlLt76C6ii03nGIqLrZ",
+  },
+  {
+    title: "Moocking Bird",
+    artist: "Eminem",
+    image: require(".//../../public/songs/floods.jpg"),
+    link: "https://open.spotify.com/track/561jH07mF1jHuk7KlaeF0s",
+  },
+  {
+    title: "Bewafa",
+    artist: "Imran Khan",
+    image: require(".//../../public/songs/nice-and-good.jpg"),
+    link: "https://open.spotify.com/track/4FcknXDJ8yW2QYUl0cm6uJ",
+  },
+  {
+    title: "Tera Mera Vivah",
+    artist: "Jass manak",
+    image: require(".//../../public/songs/care.jpg"),
+    link: "https://open.spotify.com/track/3tkxfORwPo2zydAf25YFOc",
+  },
+  {
+    title: "Mia",
+    artist: "Drake",
+    image: require(".//../../public/songs/22-double-0.jpg"),
+    link: "https://open.spotify.com/track/3Ueq2zboxwAbsvHrOjdEqz",
+  },
+];
